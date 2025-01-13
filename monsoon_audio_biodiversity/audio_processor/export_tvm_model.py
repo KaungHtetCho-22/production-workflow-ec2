bc58c2cd86c27cbd10302eac60c8c32517a0cc2b
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2025-01-13 03:59:27 +0000

    update export tvm model for more export option

--- monsoon_audio_biodiversity/audio_processor/export_tvm_model.py
+++ monsoon_audio_biodiversity/audio_processor/export_tvm_model.py
@@ -1,56 +1,57 @@
+import argparse
+import os
 import librosa
-import numpy as np
 import onnx
 import torch
-import torch.onnx
-import torch.nn as nn
 import tvm
 import tvm.relax as relax
 from tvm.relax.frontend.onnx import from_onnx
-from torchaudio.transforms import MelSpectrogram, AmplitudeToDB
 
-from monsoon_audio_biodiversity.ml_models.model import NormalizeMelSpec
+from monsoon_audio_biodiversity.ml_models.model import get_waveform_transform
 from monsoon_audio_biodiversity.audio_processor.configs.ait_bird_local import cfg as CFG
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--target', choices=['cpu', 'rasp3b'], default='cpu',
+                    help='Target processor to compile for (default: cpu)')
+parser.add_argument('--n-cpu-cores', type=int, default=8,
+                    help='Target number of CPU cores to use for compilation, ignore when cpu is rasp3b (default: 8)')
+parser.add_argument('--n-trials', type=int, default=8000,
+                    help='Target number of trials to use for static shape tuning (default: 8000)')
+parser.add_argument('--onnx-model', type=str, default='audio_classifier_model_simplified.onnx',
+                    help='Path to the ONNX model to compile (default: audio_classifier_model_simplified.onnx)')
+parser.add_argument('--output-dir', type=str, default='./',
+                    help='Path to the directory to save the compiled model (default: ./)')
+args = parser.parse_args()
 
 sample_rate = 32000
 audio_clip = librosa.load('data/audio/random-RPi-ID/2024-10-21/10-11-54_dur=600secs.wav', sr=sample_rate)
 # 5 seconds of audio
 inp_clip = torch.from_numpy(audio_clip[0][:sample_rate * 5]).unsqueeze(0)
-device = 'cpu'
+torch_device = 'cpu'
 
-logmelspec_extractor = nn.Sequential(
-    MelSpectrogram(
-        sample_rate=CFG.sample_rate,
-        n_mels=CFG.n_mels,
-        f_min=CFG.fmin,
-        f_max=CFG.fmax,
-        n_fft=CFG.n_fft,
-        hop_length=CFG.hop_length,
-        normalized=True,
-    ),
-    AmplitudeToDB(top_db=80.0),
-    NormalizeMelSpec(),
-)
+logmelspec_extractor = get_waveform_transform(CFG, torch_device)
 
 transformed_input = logmelspec_extractor(inp_clip)
 
-simplified_onnx_model = onnx.load("audio_classifier_model_simplified.onnx")
+simplified_onnx_model = onnx.load(args.onnx_model)
 
 shape_list = {'input0': (1, 1, 128, 313)}
 mod = from_onnx(simplified_onnx_model, shape_list)
 
-target = tvm.target.Target("llvm -num-cores 8")
+if args.target == 'cpu':
+    target = tvm.target.Target(f'llvm -num-cores {args.n_cpu_cores}')
+elif args.target == 'rasp3b':
+    # must use all 4 cores to get maximum speed
+    target = tvm.target.arm_cpu('rasp3b', options='-num-cores 4')
 
-TOTAL_TRIALS = 8000
-
-mod = relax.get_pipeline("static_shape_tuning", target=target, total_trials=TOTAL_TRIALS)(mod)
+mod = relax.get_pipeline("static_shape_tuning", target=target, total_trials=args.n_trials)(mod)
 
 ex = relax.build(mod, target)
-ex.export_library('llvm_8_cores_audio_classifier_model.so')
 
-device = tvm.device('cpu')
-vm = relax.VirtualMachine(ex, device)
-tmv_data = tvm.nd.array(transformed_input[:, None].numpy())
-time_ret = vm.time_evaluator("main", device, number=10)(tmv_data)
-print(f'time_ret: {time_ret}')
+cpu_cores_txt = f'-{args.n_cpu_cores}' if args.target == 'cpu' else ''
+output_file_name = f'{args.target}{cpu_cores_txt}_audio_classifier_model.so'
+output_file_path = os.path.join(args.output_dir, output_file_name)
+
+ex.export_library(output_file_path)
+print(f"TVM model exported successfully to {output_file_path}!")
